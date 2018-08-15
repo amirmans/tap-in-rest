61c4f377289d8426fd73ceffdeded57d07d73e37
Amir Amirmansoury <amir@tap-in.co>
2018-08-15 02:00:19 -0700

    more source file are using this file for db source configuration

--- include/config_db.inc.php
+++ include/config_db.inc.php
@@ -1,6 +1,32 @@
 <?php
 $db_host = 'localhost';
-$db_user = 'artdoost_dbadmin';
-$db_pass = 'id0ntknow';
-$db_name = 'artdoost_taptalk';
+$db_user = 'artdoost_tapin';
+$db_pass = 'mfood0716!!';
+$db_name = 'artdoost_tapin_prod_v2';
+
+
+// Configuration file for consumerprofile.php
+
+$config = array(
+    // These are the settings for development mode
+    'development' => array(
+        'db' => array(
+            'host'     => 'localhost',
+            'dbname'   => 'artdoost_tapin_prod_v2',
+            'username' => 'artdoost_tapin',
+            'password' => 'mfood0716!!',
+        ),
+    ),
+
+    // These are the settings for production mode
+    'production' => array(
+        'db' => array(
+            'host'     => 'localhost',
+            'dbname'   => 'artdoost_tapin_prod_v2',
+            'username' => 'artdoost_tapin',
+            'password' => 'mfood0716!!',
+        ),
+    ),
+);
+
 ?>
