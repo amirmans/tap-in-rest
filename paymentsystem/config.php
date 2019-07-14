09497bdd530c80d31949e2d27092c1f517201c11
Amir Amirmansoury <amir@tap-in.co>
2019-07-14 02:14:45 -0700

    typo

--- paymentsystem/config.php
+++ paymentsystem/config.php
@@ -2,8 +2,8 @@
 require_once('../Library/stripe/lib/Stripe.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_mBTVbAuKGx5FDk8dXXSQCay4",
-  "publishable_key" => "pk_test_zrEfGQzrGZAQ4iUqpTilP6Bi"
+  "secret_key"      => "",
+  "publishable_key" => ""
 );
 
 Stripe::setApiKey($stripe['secret_key']);
